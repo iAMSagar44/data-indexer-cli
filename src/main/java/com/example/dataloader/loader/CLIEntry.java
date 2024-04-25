9e097be7511706ddedc76e2777176f3902cb8250
Sagar Bhat <sagar.jobs@outlook.com>
2024-04-25 19:51:09 +1000

    Update code for local use (removing the logic to upload files to Azure and remove Azure dependencies)

--- src/main/java/com/example/dataloader/loader/CLIEntry.java
+++ src/main/java/com/example/dataloader/loader/CLIEntry.java
@@ -17,12 +17,10 @@ public class CLIEntry {
     private static final Logger LOGGER = LoggerFactory.getLogger(CLIEntry.class);
     private final IndexDocuments indexDocuments;
     private final JdbcClient jdbcClient;
-    private final BlobUpload blobUpload;
 
-    public CLIEntry(IndexDocuments indexDocuments, JdbcClient jdbcClient, BlobUpload blobUpload) {
+    public CLIEntry(IndexDocuments indexDocuments, JdbcClient jdbcClient) {
         this.indexDocuments = indexDocuments;
         this.jdbcClient = jdbcClient;
-        this.blobUpload = blobUpload;
     }
 
     @Command(command = "load", description = "Index documents to the Vector Store")
@@ -31,16 +29,7 @@ public class CLIEntry {
             throws IOException {
         Path folderPath = Path.of(path);
         LOGGER.info("Loading documents from the path {}", folderPath.toAbsolutePath());
-        validateFolderPath(folderPath, false);
-    }
-
-    @Command(command = "blob", description = "Load documents to an Azure Blob Storage")
-    public void loadToAzureBlobStorage(
-            @Option(longNames = "path", shortNames = 'p', required = true, label = "the path of the directory or file") String path)
-            throws IOException {
-        Path folderPath = Path.of(path);
-        LOGGER.info("Uploading documents from the path {}", folderPath.toUri());
-        validateFolderPath(folderPath, true);
+        validateFolderPath(folderPath);
     }
 
     @Command(command = "delete", description = "Deletes all the documents from the Vector Store")
@@ -50,13 +39,13 @@ public class CLIEntry {
         jdbcClient.sql("DELETE FROM vector_store").update();
     }
 
-    private void validateFolderPath(Path folderPath, boolean uploadToBlob) throws IOException {
+    private void validateFolderPath(Path folderPath) throws IOException {
         if (Files.isDirectory(folderPath)) {
             LOGGER.info("The path provided is a directory");
-            processDirectory(folderPath, uploadToBlob);
+            processDirectory(folderPath);
         } else {
             if (Files.exists(folderPath)) {
-                loadDocuments(folderPath, uploadToBlob);
+                loadDocuments(folderPath);
             } else {
                 throw new FileNotFoundException(
                         String.format("The file path provided does not exist. File Path %s", folderPath.toString()));
@@ -64,21 +53,17 @@ public class CLIEntry {
         }
     }
 
-    private void processDirectory(Path folderPath, boolean uploadToBlob) throws IOException {
+    private void processDirectory(Path folderPath) throws IOException {
         try(Stream<Path> pathStream = Files.list(folderPath)){
             final var files = pathStream.toList();
             LOGGER.info("Found {} files in the directory. Indexing each file separately. \n", files.size());
-            files.forEach(file -> loadDocuments(file, uploadToBlob));
+            files.forEach(this::loadDocuments);
         }
         
     }
 
-    private void loadDocuments(Path folderPath, boolean uploadToBlob) {
+    private void loadDocuments(Path folderPath) {
         LOGGER.info("Processing file {}", folderPath.getFileName().toString());
-        if (uploadToBlob) {
-            this.blobUpload.uploadFile(folderPath.toString());
-        } else {
             indexDocuments.load(folderPath);
-        }
     }
 }
